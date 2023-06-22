37c183f53ff83bad49d46d3b39a30aa3e70474a9
Ahmedtaher820 <et8989784@gmail.com>
2023-06-22 04:48:12 +0300

    remove pagination component

--- src/components/App/Pagination.vue
+++ src/components/App/Pagination.vue
@@ -1,21 +1,22 @@
 <script lang="ts" setup>
-import { TailwindPagination } from 'laravel-vue-pagination';
-const props = defineProps({
-    laravelData:{
+// import { TailwindPagination } from 'laravel-vue-pagination';
+// const props = defineProps({
+//     laravelData:{
 
-    }
-})
-const emits = defineEmits(['change'])
-const getResults = async (page = 1) => {
-    if (page > props.paginationMeta.last_page)
-        return
-    emits('change', page)
-}
+//     }
+// })
+// const emits = defineEmits(['change'])
+// const getResults = async (page = 1) => {
+//     if (page > props.paginationMeta.last_page)
+//         return
+//     emits('change', page)
+// }
 </script>
 
 <template>
-    <TailwindPagination
+    <div></div>
+    <!-- <TailwindPagination
         :data="laravelData"
         @pagination-change-page="getResults"
-    />
+    /> -->
 </template>
